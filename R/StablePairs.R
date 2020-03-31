2b80670340a296b7bdd494ca0ddac4131521a9e7
XJJ-student <57277449+XJJ-student@users.noreply.github.com>
2020-03-31 09:43:04 +0800

    Add files via upload

---
+++ R/StablePairs.R
@@ -0,0 +1,43 @@
+StablePairs<-
+  function(expdata_list,freq){
+    n<-length(expdata_list)
+    data1<-expdata_list[[1]]	
+	gid1<-as.matrix(data1[,1])
+	data1<-as.matrix(data1[,-1])
+	freqs=list()	
+	for(i in 1:(length(gid1)-1)){
+		cat(i,"\n")
+		gid11=gid1[-c(1:i)]
+		pair1=cbind(gid1[i],gid11)
+		coms=data1[match(pair1[,1],gid1),,drop=F]-data1[match(pair1[,2],gid1),,drop=F]
+		freq1=rowMeans(coms>0)
+		freqs[[i]]=freq1
+	}
+	pairs=t(combn(gid1,2))
+	freqs=unlist(freqs)
+	stable_pair=rbind(pairs[freqs>freq,],pairs[freqs<(1-freq),c(2,1)])
+    if(n==1){
+    return(stable_pair)
+	}
+    if(n>1){ 
+    for (k in 2:n){
+    data1<-as.matrix(expdata_list[[k]])	
+	gid1<-as.matrix(data1[,1])
+	data1<-as.matrix(data1[,-1])
+	freqs=list()
+	for(i in 1:(length(gid1)-1)){
+		cat(i,"\n")
+		gid11=gid1[-c(1:i)]
+		pair1=cbind(gid1[i],gid11)
+		coms=data1[match(pair1[,1],gid1),,drop=F]-data1[match(pair1[,2],gid1),,drop=F]
+		freq1=rowMeans(coms>0)
+		freqs[[i]]=freq1
+	}
+	pairs=t(combn(gid1,2))
+	freqs=unlist(freqs)
+	stablepair=rbind(pairs[freqs>freq,],pairs[freqs<(1-freq),c(2,1)])
+	stable_pair<-merge(stable_pair,stablepair)
+    }
+    return(stable_pair)
+  }	
+}
