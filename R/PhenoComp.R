ec44aecb841a18bfca642fe00494a638e1d43b36
Alex Pickering <alexvpickering@gmail.com>
2020-10-12 12:35:37 -0700

    optimize StablePairs; switch to roxygen2; cleanup syntax
    StablePairs optimization:
    1) generation of pairs using combn is slow for a large number of genes. If genes
    are the same in each control dataset then it only needs to be generated once.
    
    2) base merge is very slow for large matrices. Switch to data.table's speeds this up ~38x.
    
    3) if row.names are set on matrices in expdata_list then freqs will take ~2x more RAM. A quick
    scrub of row.names prevents this.
    
    Other:
    1) switch to roxygen2 for automated documentation
    2) repace use of '=' for assignment with '<-', add space after commas, etc.

--- R/PhenoComp.R
+++ R/PhenoComp.R
@@ -1,137 +1,168 @@
-PhenoComp<-
-  function(stable_pair,expE,individualfdr,method,populationfdr){
-	gene<-expE[,1]
-	con_gid=intersect(unique(c(stable_pair[,1],stable_pair[,2])),gene);
-	expE<-expE[match(con_gid,gene),-1]
-	gene=con_gid
-	index1<-stable_pair[,1] %in% gene 
-	index2<-stable_pair[,2] %in% gene 
-	stable_pair=stable_pair[index1+index2==2,]	
-	case_exp<-expE
-	cat("differential expressed analysis... \n");
-	outlier_dir<-NULL;
-    outlier_pvalue<-NULL;
-    Lgene<-length(gene);
-    for (k in 1:Lgene){
-      cat(k,"\n")
-      colC<-dim(case_exp)[2]
-	  Nloc_up=stable_pair[stable_pair[,1] %in% gene[k],2]
-	  Nloc_down=stable_pair[stable_pair[,2] %in% gene[k],1]	  	  
-	  reverse=matrix(0,colC,4)
-	  reverse[,1]=rep(length(Nloc_up),colC)
-	  reverse[,2]=rep(length(Nloc_down),colC)
-	  Tcanc=case_exp[k,]
-      if (length(Nloc_up)>0){
-	    Nloc_up1<-match(Nloc_up,gene)
-        N_tmp=matrix(rep(Tcanc,length(Nloc_up)),ncol=colC,byrow=T)-case_exp[Nloc_up1,]
-        case_p=colSums(N_tmp<0)
-        reverse[,3]=case_p
-      }
-      if (length(Nloc_down)>0){	    
-	    Nloc_down1<-match(Nloc_down,gene)
-        N_tmpp=matrix(rep(Tcanc,length(Nloc_down)),ncol=colC,byrow=T)-case_exp[Nloc_down1,]
-        case_pp=colSums(N_tmpp>0)
-        reverse[,4]=case_pp; 
-      }	    
-      GenePair_sig=NULL
-      GenePair=rep(0,colC)
-      GenePair[which(reverse[,3]>reverse[,4])]<--1
-      GenePair[which(reverse[,3]<reverse[,4])]<-1
-      tmp=matrix(c(reverse[,1],reverse[,2],reverse[,1]-reverse[,3]+reverse[,4], reverse[,2]-reverse[,4]+reverse[,3]),ncol=4)
-      GenePair_sig<-apply(tmp,1,function(x) fisher.test(matrix(x,ncol=2,byrow=T))$p.value)
-      outlier_dir=rbind(outlier_dir,GenePair)
-      outlier_pvalue=rbind(outlier_pvalue,GenePair_sig)
+#' Identification of population-level differentially expressed genes in one-phenotype data
+#'
+#' PhenoComp is an algorithm to identify population-level differential genes
+#' in one-phenotype data. This algorithm is based on RankComp,  an algorithm
+#' used to identify individual-level differentially expressed genes in each
+#' sample.
+#'
+#' @param stable_pair Stable gene pairs identified by the function of StablePairs.
+#' @param expE A (non-empty) numeric matrix of disease samples.
+#'  The first columns is the Entrez gene IDs and the remaining columns
+#'  are the gene expression values of the disease samples.
+#' @param individualfdr The threshold of FDR for identifying individual-level
+#'  differentially expressed genes
+#' @param method Method determines how to estimate the p_up and p_down.
+#'  Method=1: the p_up and p_down were estimated as the median values of
+#'  the frequencies of up-regulated and down-regulated genes for individual
+#'  disease samples.Method=2: the p_up and p_down were estimated as the mean
+#'  values of the frequencies of up-regulated and down-regulated genes for
+#'  individual disease samples.
+#' @param populationfdr The threshold of FDR for identifying population-level
+#'  differentially expressed genes.
+#'
+#' @return List with upregulated and downregulated gene IDs.
+#' @export
+#'
+#' @examples
+#'
+#' data(example)
+#' expdata_list=list(normalexp1, normalexp2)
+#' stable_pair=StablePairs(expdata_list, 0.99)
+#' PhenoComp(stable_pair, expE, 0.05, 1, 0.05)
+#'
+PhenoComp <- function(stable_pair, expE, individualfdr, method, populationfdr){
+  gene <- expE[, 1]
+  con_gid <- intersect(unique(c(stable_pair[, 1], stable_pair[, 2])), gene)
+  expE <- expE[match(con_gid, gene), -1]
+  gene <- con_gid
+  index1 <- stable_pair[, 1] %in% gene
+  index2 <- stable_pair[, 2] %in% gene
+  stable_pair <- stable_pair[index1+index2==2, ]
+  case_exp <- expE
+  cat("differential expressed analysis... \n")
+  outlier_dir <- NULL
+  outlier_pvalue <- NULL
+  Lgene <- length(gene)
+  for (k in 1:Lgene){
+    cat(k, "\n")
+    colC <- dim(case_exp)[2]
+    Nloc_up <- stable_pair[stable_pair[, 1] %in% gene[k], 2]
+    Nloc_down <- stable_pair[stable_pair[, 2] %in% gene[k], 1]
+    reverse <- matrix(0, colC, 4)
+    reverse[, 1] <- rep(length(Nloc_up), colC)
+    reverse[, 2] <- rep(length(Nloc_down), colC)
+    Tcanc=case_exp[k, ]
+    if (length(Nloc_up)>0){
+      Nloc_up1 <- match(Nloc_up, gene)
+      N_tmp <- matrix(rep(Tcanc, length(Nloc_up)), ncol=colC, byrow=T)-case_exp[Nloc_up1, ]
+      case_p <- colSums(N_tmp<0)
+      reverse[, 3] <- case_p
     }
-    fdr<-apply(outlier_pvalue,2,function(x) p.adjust(x,method="fdr",length(x)))
-
-	individual_level_DEGs<-matrix(0,nrow(outlier_dir),ncol(outlier_dir))
-	for(i in 1:ncol(outlier_dir)){
-	a<-which(fdr[,i]<individualfdr)
-	individual_level_DEGs[a,i]<-outlier_dir[a,i]
-	}
-      p_diff<-c()
-      for(i in 1:ncol(individual_level_DEGs)){
-         want_col_DEG<-individual_level_DEGs[,i]
-         up_num<-sum(want_col_DEG==1)
-         down_num<-sum(want_col_DEG==-1)
-         all_num<-nrow(individual_level_DEGs)
-         p_diff1<-(up_num+down_num)/all_num
-         p_diff<-c(p_diff,p_diff1)
-          }
-	  p_up_result<-c()
-      for(i in 1:ncol(individual_level_DEGs)){
-         want_col_DEG<-individual_level_DEGs[,i]
-         up_num<-sum(want_col_DEG==1)
-         down_num<-sum(want_col_DEG==-1)
-         p_up_ratio<-up_num/(up_num+down_num)
-         p_up_result<-c(p_up_result,p_up_ratio)
-          }
-	  if(method==1){
-        p0_up<-(median(p_diff))*(median(p_up_result))
-		}
-	  if(method==2){
-        p0_up<-(mean(p_diff))*(mean(p_up_result))
-		}
-	  up_result<-matrix(0,dim(individual_level_DEGs)[1],4)
-      z<-1
-      for(i in 1:dim(individual_level_DEGs)[1]){
-        k<-sum(individual_level_DEGs[i,]==1)
-        s<-dim(individual_level_DEGs)[2]
-        up_result[z,2]<-k/s
-        up_result[z,3]<-1-pbinom(k-1,s,p0_up)
-        z<-z+1
-      }
-      up_result[,1]<-gene
-      up_result[,4]=p.adjust(up_result[,3],method="BH")
-      colnames(up_result)<-c("geneid","up_ratio","p.value","FDR")
-      up_DEG<-up_result[which(up_result[,4]<populationfdr),1]
-      cat(length(up_DEG))
-	  p_down_result<-c()
-      for(i in 1:ncol(individual_level_DEGs)){
-        want_col_DEG<-individual_level_DEGs[,i]
-        up_num<-sum(want_col_DEG==1)
-        down_num<-sum(want_col_DEG==-1)
-        p_down_ratio<-down_num/(up_num+down_num)
-        p_down_result<-c(p_down_result,p_down_ratio)
-        }
-	 if(method==1){
-        p0_down<-(median(p_diff))*(median(p_down_result))
-		}
-	  if(method==2){
-        p0_down<-(mean(p_diff))*(mean(p_down_result))
-		}
-      down_result<-matrix(0,dim(individual_level_DEGs)[1],4)
-      z<-1
-      for(i in 1:dim(individual_level_DEGs)[1]){
-        k<-sum(individual_level_DEGs[i,]==-1)
-        s<-dim(individual_level_DEGs)[2]
-        down_result[z,2]<-k/s
-        down_result[z,3]<-1-pbinom(k-1,s,p0_down)
-        z<-z+1
-      }
-      down_result[,1]<-gene
-      down_result[,4]=p.adjust(down_result[,3],method="BH")
-      colnames(down_result)<-c("geneid","down_ratio","p.value","FDR")
-      down_DEG<-down_result[which(down_result[,4]<populationfdr),1]
-	  cat(length(down_DEG))
-      overlap_gene<-intersect(up_DEG,down_DEG)
-	  overlap_gene_num<-length(overlap_gene)
-      cat(overlap_gene_num)
-	  if(overlap_gene_num==0){
-	  cat(length(up_DEG)+length(down_DEG))
-	  population_DEGs<-list(up_DEG,down_DEG)
-	  return(population_DEGs)
-	  }
-	  if(overlap_gene_num>0){
-	  up_gene<-up_DEG[-match(overlap_gene,up_DEG)]
-      down_gene<-down_DEG[-match(overlap_gene,down_DEG)]
-      cat(length(up_gene))
-	  cat(length(down_gene))
-	  cat(length(up_gene)+length(down_gene))
-	  population_DEGs<-list(up_gene,down_gene)
-	  return(population_DEGs) 
-      }
+    if (length(Nloc_down)>0){
+      Nloc_down1 <- match(Nloc_down, gene)
+      N_tmpp <- matrix(rep(Tcanc, length(Nloc_down)), ncol=colC, byrow=T)-case_exp[Nloc_down1, ]
+      case_pp <- colSums(N_tmpp>0)
+      reverse[, 4] <- case_pp;
+    }
+    GenePair_sig <- NULL
+    GenePair <- rep(0, colC)
+    GenePair[which(reverse[, 3]>reverse[, 4])] <- -1
+    GenePair[which(reverse[, 3]<reverse[, 4])] <-  1
+    tmp <- matrix(c(reverse[, 1], reverse[, 2], reverse[, 1]-reverse[, 3]+reverse[, 4],  reverse[, 2]-reverse[, 4]+reverse[, 3]), ncol=4)
+    GenePair_sig <- apply(tmp, 1, function(x) fisher.test(matrix(x, ncol=2, byrow=T))$p.value)
+    outlier_dir <- rbind(outlier_dir, GenePair)
+    outlier_pvalue <- rbind(outlier_pvalue, GenePair_sig)
   }
+  fdr <- apply(outlier_pvalue, 2, function(x) p.adjust(x, method="fdr", length(x)))
+
+  individual_level_DEGs <- matrix(0, nrow(outlier_dir), ncol(outlier_dir))
+  for(i in 1:ncol(outlier_dir)){
+    a <- which(fdr[, i]<individualfdr)
+    individual_level_DEGs[a, i] <- outlier_dir[a, i]
+  }
+  p_diff <- c()
+  for(i in 1:ncol(individual_level_DEGs)){
+    want_col_DEG <- individual_level_DEGs[, i]
+    up_num <- sum(want_col_DEG==1)
+    down_num <- sum(want_col_DEG==-1)
+    all_num <- nrow(individual_level_DEGs)
+    p_diff1<-(up_num+down_num)/all_num
+    p_diff <- c(p_diff, p_diff1)
+  }
+  p_up_result <- c()
+  for(i in 1:ncol(individual_level_DEGs)){
+    want_col_DEG <- individual_level_DEGs[, i]
+    up_num <- sum(want_col_DEG==1)
+    down_num <- sum(want_col_DEG==-1)
+    p_up_ratio <- up_num/(up_num+down_num)
+    p_up_result <- c(p_up_result, p_up_ratio)
+  }
+  if(method==1){
+    p0_up <- (median(p_diff))*(median(p_up_result))
+  }
+  if(method==2){
+    p0_up <- (mean(p_diff))*(mean(p_up_result))
+  }
+  up_result <- matrix(0, dim(individual_level_DEGs)[1], 4)
+  z <- 1
+  for(i in 1:dim(individual_level_DEGs)[1]){
+    k <- sum(individual_level_DEGs[i, ]==1)
+    s <- dim(individual_level_DEGs)[2]
+    up_result[z, 2] <- k/s
+    up_result[z, 3] <- 1-pbinom(k-1, s, p0_up)
+    z <- z+1
+  }
+  up_result[, 1] <- gene
+  up_result[, 4] <- p.adjust(up_result[, 3], method="BH")
+  colnames(up_result) <- c("geneid", "up_ratio", "p.value", "FDR")
+  up_DEG <- up_result[which(up_result[, 4]<populationfdr), 1]
+  cat(length(up_DEG))
+  p_down_result <- c()
+  for(i in 1:ncol(individual_level_DEGs)){
+    want_col_DEG <- individual_level_DEGs[, i]
+    up_num <- sum(want_col_DEG==1)
+    down_num <- sum(want_col_DEG==-1)
+    p_down_ratio <- down_num/(up_num+down_num)
+    p_down_result <- c(p_down_result, p_down_ratio)
+  }
+  if(method==1){
+    p0_down <- (median(p_diff))*(median(p_down_result))
+  }
+  if(method==2){
+    p0_down <- (mean(p_diff))*(mean(p_down_result))
+  }
+  down_result <- matrix(0, dim(individual_level_DEGs)[1], 4)
+  z <- 1
+  for(i in 1:dim(individual_level_DEGs)[1]){
+    k <- sum(individual_level_DEGs[i, ]==-1)
+    s <- dim(individual_level_DEGs)[2]
+    down_result[z, 2] <- k/s
+    down_result[z, 3] <- 1-pbinom(k-1, s, p0_down)
+    z <- z+1
+  }
+  down_result[, 1] <- gene
+  down_result[, 4] <- p.adjust(down_result[, 3], method="BH")
+  colnames(down_result) <- c("geneid", "down_ratio", "p.value", "FDR")
+  down_DEG <- down_result[which(down_result[, 4]<populationfdr), 1]
+  cat(length(down_DEG))
+  overlap_gene <- intersect(up_DEG, down_DEG)
+  overlap_gene_num <- length(overlap_gene)
+  cat(overlap_gene_num)
+  if(overlap_gene_num==0){
+    cat(length(up_DEG)+length(down_DEG))
+    population_DEGs <- list(up=up_DEG, down=down_DEG)
+    return(population_DEGs)
+  }
+  if(overlap_gene_num>0){
+    up_gene <- up_DEG[-match(overlap_gene, up_DEG)]
+    down_gene <- down_DEG[-match(overlap_gene, down_DEG)]
+    cat(length(up_gene))
+    cat(length(down_gene))
+    cat(length(up_gene)+length(down_gene))
+    population_DEGs <- list(up=up_gene, down=down_gene)
+    return(population_DEGs)
+  }
+}
 
 
 
--- R/StablePairs.R
+++ R/StablePairs.R
@@ -1,43 +1,80 @@
-StablePairs<-
-  function(expdata_list,freq){
-    n<-length(expdata_list)
-    data1<-expdata_list[[1]]	
-	gid1<-as.matrix(data1[,1])
-	data1<-as.matrix(data1[,-1])
-	freqs=list()	
-	for(i in 1:(length(gid1)-1)){
-		cat(i,"\n")
-		gid11=gid1[-c(1:i)]
-		pair1=cbind(gid1[i],gid11)
-		coms=data1[match(pair1[,1],gid1),,drop=F]-data1[match(pair1[,2],gid1),,drop=F]
-		freq1=rowMeans(coms>0)
-		freqs[[i]]=freq1
-	}
-	pairs=t(combn(gid1,2))
-	freqs=unlist(freqs)
-	stable_pair=rbind(pairs[freqs>freq,],pairs[freqs<(1-freq),c(2,1)])
-    if(n==1){
+
+#' Identification of population-level differentially expressed genes in one-phenotype data
+#'
+#' The StablePairs is to identify stable gene pairs by using the gene expression profile list of the normal samples of one human tissue-type.
+#'
+#' @param expdata_list The gene expression profile list of the normal samples of
+#'  one human tissue-type which is measured by one or more platforms. The first
+#'  columns is the gene IDs and the remaining columns are the gene expression
+#'  values of the normal samples.
+#' @param freq The criteria for identifying stable gene pairs in normal samples.
+#'  The default setting of freq is 0.99.
+#'
+#' @return matrix with each row indicating a stable pair of
+#'  gene IDs across the datasets in \code{expdata_list}.
+#' @export
+#'
+#' @examples
+#'
+#' data(example)
+#' expdata_list <- list(normalexp1,normalexp2)
+#' stable_pair <- StablePairs(expdata_list)
+#'
+StablePairs  <-  function(expdata_list, freq = 0.99){
+  n <- length(expdata_list)
+  data1 <- expdata_list[[1]]
+  gid1 <- as.matrix(data1[,1])
+
+  # row names not needed and double memory usage of freqs
+  expdata_list <- lapply(expdata_list, function(x) {row.names(x) <- NULL; x})
+
+
+  # one-time generation of pairs if all in same order
+  gids <- lapply(expdata_list, function(x) x[,1])
+  gids_eq <- all(sapply(gids, identical, gids[[1]]))
+
+  pairs <-  t(combn(gid1,2))
+  data1 <- as.matrix(data1[,-1])
+
+  freqs=list()
+  for(i in 1:(length(gid1)-1)){
+    cat(i,"\n")
+    gid11 <- gid1[-c(1:i)]
+    pair1 <- cbind(gid1[i],gid11)
+    coms <- data1[match(pair1[,1],gid1),,drop=F]-data1[match(pair1[,2],gid1),,drop=F]
+    freq1 <- rowMeans(coms>0)
+    freqs[[i]] <- freq1
+  }
+  freqs <- unlist(freqs)
+  stable_pair <- rbind(pairs[freqs>freq,],pairs[freqs<(1-freq),c(2,1)])
+  if(n==1){
     return(stable_pair)
-	}
-    if(n>1){ 
+  }
+  if(n>1){
+    # speed up merges
+    stable_pair <- data.table::data.table(stable_pair)
+
     for (k in 2:n){
-    data1<-as.matrix(expdata_list[[k]])	
-	gid1<-as.matrix(data1[,1])
-	data1<-as.matrix(data1[,-1])
-	freqs=list()
-	for(i in 1:(length(gid1)-1)){
-		cat(i,"\n")
-		gid11=gid1[-c(1:i)]
-		pair1=cbind(gid1[i],gid11)
-		coms=data1[match(pair1[,1],gid1),,drop=F]-data1[match(pair1[,2],gid1),,drop=F]
-		freq1=rowMeans(coms>0)
-		freqs[[i]]=freq1
-	}
-	pairs=t(combn(gid1,2))
-	freqs=unlist(freqs)
-	stablepair=rbind(pairs[freqs>freq,],pairs[freqs<(1-freq),c(2,1)])
-	stable_pair<-merge(stable_pair,stablepair)
+      data1 <- as.matrix(expdata_list[[k]])
+      gid1 <- as.matrix(data1[,1])
+      data1 <- as.matrix(data1[,-1])
+      freqs <- list()
+      for(i in 1:(length(gid1)-1)){
+        cat(i,"\n")
+        gid11 <- gid1[-c(1:i)]
+        pair1 <- cbind(gid1[i],gid11)
+        coms <- data1[match(pair1[,1],gid1),,drop=F]-data1[match(pair1[,2],gid1),,drop=F]
+        freq1 <- rowMeans(coms>0)
+        freqs[[i]] <- freq1
+      }
+      if (!gids_eq) pairs <- t(combn(gid1,2))
+      freqs <- unlist(freqs)
+      stablepair <- rbind(pairs[freqs>freq,],pairs[freqs<(1-freq),c(2,1)])
+
+      stablepair <- data.table::data.table(stablepair)
+      stable_pair <- merge(stable_pair,stablepair)
+
     }
-    return(stable_pair)
-  }	
+    return(as.matrix(stable_pair))
+  }
 }
